44e19966a1903afe1992df6e65e3778458a1560d
Chaitri S.M <chaitri.s.mohan@gmail.com>
2017-08-07 21:53:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res) {
+   res.send('Article 1 is being developed'); 
+});
+
+app.get('/article-two', function (req,res) {
+   res.send('Article 2 is being developed'); 
+});
+
+app.get('/article-three', function (req,res) {
+   res.send('Article 3 is being developed'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
