a818617a0c8377e8a538df56c5efbcb28e8bbd1e
Chaitri S.M <chaitri.s.mohan@gmail.com>
2017-08-09 19:47:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 var element = document.getElementById('name');
 element.innerHTML = 'Chaitri SM';
 element.onClick = function() {
-    element.style.marginLeft = '100px';
+    element.style.marginLeft = '900px';
 };
